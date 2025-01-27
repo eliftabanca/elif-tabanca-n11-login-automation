cac1c3880ee854f493aa20faf9bf89131ef91757
eliftabanca <elifbusratabanca@gmail.com>
2025-01-27 10:58:17 +0300

    Optimize wait functions for better maintainability using centralized WebDriverWait (EC)

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,41 +15,28 @@ class BasePage:
     def open_url(self, url):
         self.driver.get(url)
 
-    # # Waits for an element to be present and returns it
-    # def find_element(self, locator, timeout=10):
-    #     wait = WebDriverWait(self.driver, timeout)
-    #     return wait.until(EC.presence_of_element_located(locator))
-    
-    
+
     def wait_element_visibility(self, locator):
-        element = WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(locator))
-        return element
-    
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
     def wait_element_presence(self, locator):
-        element = WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located(locator))
-        return element
-   
-    def wait_element_clickable(self, locator):
-        element = WebDriverWait(self.driver, 30).until(expected_conditions.element_to_be_clickable(locator))
-        return element
+        return self.wait.until(EC.presence_of_element_located(locator))
     
+    
+    def click_element(self, locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
 
-    def click_element(self, locator): #checks both: clickable and clicks
-
-         element = self.wait.until(EC.element_to_be_clickable(locator))
-         element.click()
+    def wait_element_clickable(self, locator):
+        return self.wait.until(EC.element_to_be_clickable(locator))
 
     def enter_text(self, locator, text):
-        # Waits for an element to be present and enters the specified text
         element = self.find_element(locator)
         element.clear()
         element.send_keys(text)
 
     def get_text(self, locator):
-        # Waits for an element to be present and returns its text
         return self.find_element(locator).text
-
-
 
     def take_screenshot(self, filename="screenshot"):
         if not os.path.exists("screenshots"):
