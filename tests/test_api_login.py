393aaa25fb7ae548eef8ec9ae6c785a5b272ef9d
eliftabanca <elifbusratabanca@gmail.com>
2025-01-28 00:31:03 +0300

    Added valid user API login test.

---
+++ tests/test_api_login.py
@@ -0,0 +1,31 @@
+import requests
+import cloudscraper
+import unittest
+from utils.api_constants import *
+
+class TestN11Login(unittest.TestCase):
+
+    def test_valid_login(self):
+        url = BASE_URL_API
+        payload = {
+            'email': VALID_USER_EMAIL,
+            'password': VALID_USER_PASSWORD,
+            'rememberMe': 'on',
+            'returnUrl': '/hesabim/siparislerim'
+        }
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        # Bypassing Cloudflare Protection with Cloudscraper Python library
+        # Issue: The Cloudflare system used by N11 checks whether incoming requests 
+        # come from a bot or a real browser. The standard requests library cannot 
+        # bypass these security checks, which resulted in a 403 (Forbidden) error.
+        # Solution: We integrated the cloudscraper library, which automatically generates 
+        # the required tokens and cookies to bypass Cloudflare's security mechanisms.
+        scraper = cloudscraper.create_scraper()
+        response = scraper.post(url, headers=headers, data=payload)
+
+        self.assertEqual(response.status_code, 200,
+                          f"The login was expected to succeed, but the status code was: {response.status_code}"
+        )
---
+++ utils/api_constants.py
@@ -0,0 +1,3 @@
+BASE_URL_API = "https://www.n11.com/giris-yap"
+VALID_USER_EMAIL = "validusertest01@gmail.com"
+VALID_USER_PASSWORD = "Password123"
